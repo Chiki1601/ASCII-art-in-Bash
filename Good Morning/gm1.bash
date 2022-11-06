010ac4a54f8ea794d375cae7c9686b85485390eb
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 11:22:43 +0530

    Create gm1.bash

---
+++ Good Morning/gm1.bash
@@ -0,0 +1,20 @@
+
+# good morning
+     
+echo "🌷🌹🍃🌹🌷G ";
+echo "┏╮╭┓┏╮╭┓ O ";
+echo "╰╭╮╯╰╭╮╯ O ";
+echo "╭╰╯╮╭╰╯╮ D ";
+echo "┗╯╰┛┗╯╰┛  ";
+echo "🌷🌹🍃🌹🌷 M ";
+echo "┏╮╭┓┏╮╭┓  O ";
+echo "╰╭╮╯╰╭╮╯  R ";
+echo "╭╰╯╮╭╰╯╮  N ";
+echo "┗╯╰┛┗╯╰┛   I ";
+echo "🌷🌹🍃🌹🌷 N ";
+echo "┏╮╭┓┏╮╭┓  G ";
+echo "╰╭╮╯╰╭╮╯   ";
+echo "╭╰╯╮╭╰╯╮  D ";
+echo "┗╯╰┛┗╯╰┛  E ";
+echo "🌷🌹🍃🌹🌷 A ";
+echo "      FRIEND      ";
